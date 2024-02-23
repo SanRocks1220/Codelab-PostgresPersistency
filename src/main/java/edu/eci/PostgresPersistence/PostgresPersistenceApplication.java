d0d121386931042fbb249a8da274fc97f8999a41
SanRocks1220 <xxsantirocha12xx@gmail.com>
2024-02-22 22:10:49 -0500

    Configuraciones añadidas

---
+++ src/main/java/edu/eci/PostgresPersistence/PostgresPersistenceApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.PostgresPersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresPersistenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/PostgresPersistence/PostgresPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.PostgresPersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
